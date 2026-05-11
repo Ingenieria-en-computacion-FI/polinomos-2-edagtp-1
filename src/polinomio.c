97612b6dd738b76cbf01b6b9f0cede8a4b8f1156
Daniel Alejandro Gonzalez Morales <alejandrodaniel1525@gmail.com>
2026-05-11 23:39:32 +0000

    subo polinomio.c

--- src/polinomio.c
+++ src/polinomio.c
@@ -15,7 +15,7 @@
 #include <stdlib.h>
 #include <math.h>
 #include <string.h>
-#include "polinomio.h"
+#include "../include/polinomio.h"
 
 /* ═══════════════════════════════════════════════════════════════════════════
  *  LISTA
